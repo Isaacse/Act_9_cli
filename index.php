c304154165230e6f6b28d236be07eaa665614771
Isaacse <125591950+Isaacse@users.noreply.github.com>
2023-06-16 09:28:22 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+      $x= "55";
+      $y= "10.5";
+      $xy= $x + $y;
+
+      echo "<h2> $x + $y = $xy";
+?>
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
